dd8b15d2032e17147a51122034f89d4119147906
petroif <petro.dolyk@gmail.com>
2021-10-31 16:56:39 +0200

    add office info

--- js/contacts-offices.js
+++ js/contacts-offices.js
@@ -1,33 +1,6 @@
-(function() {
-    const offices = [
-        {
-            id: '1',
-            title:  'Head office in London',
-            image: 'img/contacts/offices.jpg',
-            address: 'Level 17, Dashwood House',
-            cityCoordinate: 'London EC2M 1QS',
-            phoneNumber: '+1 (234) 567 89 00',
-            officeMail: 'mywebsite@email.com'  
-        },
-        {
-            id: '2',
-            title:  'Vienna office',
-            image: 'img/contacts/offices.jpg',
-            address: 'Kaerntner Ring 5-7',
-            cityCoordinate: '1010 Vienna',
-            phoneNumber: '+1 (234) 567 89 00',
-            officeMail: 'mywebsite@email.com'  
-        },
-        {
-            id: '3',
-            title:  'Amsterdam office',
-            image: 'img/contacts/offices.jpg',
-            address: 'Keizersgracht 62-64',
-            cityCoordinate: '1015 CS Amsterdam',
-            phoneNumber: '+1 (234) 567 89 00',
-            officeMail: 'mywebsite@email.com'  
-        }
-    ];
+(async function() {
+    const response = await fetch('offices.json');
+    const offices = await response.json();
 
     function renderOffices (offices) {
         const officesContainer = document.querySelector('.mainOffice');
@@ -38,12 +11,17 @@
                     <img src="${office.image}" alt="${office.title}">
                     <p>${office.address}</p>
                     <p>${office.cityCoordinate}</p>
-                    <p class="timeNow"></p>
                     <a href="tel:${office.phoneNumber}" data-action="phone" class="phone-link">${office.phoneNumber}</a><br>
                     <a href="mailto:${office.officeMail}" data-action="email" class="email-link">${office.officeMail}</a>
+                    <form action="pages/office-info.html" target="_blank">
+                        <button type="submit" class="btn btn-light office-info-btn">
+                            <span class="button-learn-text">More...</span>
+                        </button>
+                    </form> 
                 </article>
             `;
         }
+        document.querySelectorAll('.office-info-btn')
         
     }
 
