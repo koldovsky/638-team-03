a490c9b3617b0c5417a46695708ef95c84a008ec
Oleh Mamalyha <omamalyha@gmail.com>
2021-10-31 18:18:02 +0200

    carousel works

--- js/footer-carousel.js
+++ js/footer-carousel.js
@@ -1,11 +1,11 @@
 (function () {
     const slides =[
-        `<img class="img-in-footer-carousel"src="img/imgs-above-footer/girl-in-white.webp" alt="girl-in-white">`,
-        `<img class="img-in-footer-carousel"src="img/imgs-above-footer/black-bras.webp" alt="black-bras">`,
-        `<img class="img-in-footer-carousel"src="img/imgs-above-footer/girl-on-balcony.webp" alt="girl-on-balcony">`,
-        `<img class="img-in-footer-carousel"src="img/imgs-above-footer/two-girls-in-black.webp" alt="two-girls-in-black">`,
-        `<img class="img-in-footer-carousel"src="img/imgs-above-footer/one-girl-in-black.webp" alt="one-girl-in-black">`,
-        `<img class="img-in-footer-carousel"src="img/imgs-above-footer/girl-in-shirt.webp" alt="girl-in-white-shirt">`
+        `<div><img class="img-in-footer-carousel"src="img/imgs-above-footer/girl-in-white.webp" alt="girl-in-white"></div`,
+        `<div><img class="img-in-footer-carousel"src="img/imgs-above-footer/black-bras.webp" alt="black-bras"></div`,
+        `<div><img class="img-in-footer-carousel"src="img/imgs-above-footer/girl-on-balcony.webp" alt="girl-on-balcony"></div`,
+        `<div><img class="img-in-footer-carousel"src="img/imgs-above-footer/two-girls-in-black.webp" alt="two-girls-in-black"></div`,
+        `<div><img class="img-in-footer-carousel"src="img/imgs-above-footer/one-girl-in-black.webp" alt="one-girl-in-black"></div`,
+        `<div><img class="img-in-footer-carousel"src="img/imgs-above-footer/girl-in-shirt.webp" alt="girl-in-white-shirt"></div`
     ];
 
     let currentSlide = 0;
@@ -16,6 +16,10 @@
         if (window.innerWidth > 480) {
             const secondSlideIdx = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
             slideContainer.innerHTML += slides[secondSlideIdx];
+            if (window.innerWidth > 770) {
+                const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1
+                slideContainer.innerHTML += slides[thirdSlideIdx]
+            }
             
         }
 
@@ -35,8 +39,10 @@
 
     document.querySelector(".footer-carousel-js .next-footer-slide").addEventListener("click", nextSlide)
     document.querySelector(".footer-carousel-js .prev-footer-slide").addEventListener("click", prevSlide)
+
     
     showCurrentSlide();
-
+    
+    window.addEventListener('resize', showCurrentSlide)
 
 }) ();
